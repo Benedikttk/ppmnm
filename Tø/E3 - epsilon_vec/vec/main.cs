a1a3b73dde8b69c170caac8a9eb2b80a0b0ac272
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-02-15 10:25:34 +0100

    hej

--- Tø/E3 - epsilon_vec/vec/main.cs
+++ Tø/E3 - epsilon_vec/vec/main.cs
@@ -1,17 +1,23 @@
 using System;
 using static System.Console;
 public static class main{
+	
 	public static void Main(){
+		///Task1 and Task2 have been done in vec.cs.
+		///Task3 as also been done, as the different opperators.
 
+		WriteLine("The public class vec, can be found in vec.cs and is read through the vec.llb. There you can see all of its opperations.");
+
+		
 		vec u = new vec(1,2,3);
 		vec v = new vec(2,3,4);
+	
+		WriteLine($"We start by adding to vectors together, vector u and vector v.");
 		u.print("u=");
 		v.print("v=");
 		(u+v).print("u+v=");
 
-		
-
-
+		WriteLine("\n We now define 2 more vectors - a, b. and subtract them and make one of them negative.");
 		vec a = new vec(3,4,5);
 		vec b = new vec(4,5,6);
 		a.print("a=");
@@ -20,13 +26,20 @@ public static class main{
 		(a-b).print("a-b=");
 		(-a).print("-a=");
 		
-		
+		WriteLine("\n Now we see if we can do scallar multiplication on our vectors, with a scallar, w=a*5. Note that if using (5*a).print this will give a value, but (a*5).print will grant an error as it is a bug in all c programs.");
 		var w = a*5;
 		w.print("a*5=");
-		WriteLine($"a%b={a%b}");
-
-		///approx
 		
+		///Task 5 - test
+		WriteLine($"\n We are now going to find the dot product between two vectors, a, v-> dot(a,v)={vec.dot(a,v)}");
+
+		
+		///Task 6 -test
+		WriteLine("\n We will now be testing to see if the approximation works on our vectors and scallars.");
+		
+		///Task6
+
+		var test_2_vec = vec.approx(a,b);
 
 
 
--- Tø/E3 - epsilon_vec/vec/vec.cs
+++ Tø/E3 - epsilon_vec/vec/vec.cs
@@ -1,44 +1,55 @@
+using System;
 using static System.Console;
 using static System.Math;
 
-
+///Task1 and Task2 
 public class vec{
 	public double x,y,z;
+	public vec (){ x=y=z=0; }
 	public vec (double x,double y,double z)
 		{ this.x=x; this.y=y; this.z=z; }
-	public vec (){ x=y=z=0; }
+///Task 4
 	public void print(string s){Write(s);WriteLine($"{x} {y} {z}");}
+	public void print(){this.print("");}
+	
+	
+///Task3 start
 
-	public static vec operator+(vec u,vec v){ /* u+v */
+	//Adding
+	public static vec operator+(vec u,vec v){
 		return new vec(u.x+v.x,u.y+v.y,u.z+v.z);
 			}
-
-	public static vec operator-(vec u,vec v){ /* u-v */
+	//Subtracting
+	public static vec operator-(vec u,vec v){
 		return new vec(u.x-v.x,u.y-v.y,u.z-v.z);
 			}
-
-	public static vec operator-(vec v){ /* -v */
+	//making negative
+	public static vec operator-(vec v){ 
 		return new vec(-v.x,-v.y,-v.z);
 			}
-
-	public static vec operator*(vec u,double c){ /* u*c */
+	//multiplication of scallar
+	public static vec operator*(vec u,double c){
 		return new vec(u.x*c,u.y*c,u.z*c);
 			}
-
-	public static vec operator*(double c,vec u){ /* c,*u */
+	//multiplication of scallar
+	public static vec operator*(double c,vec u){
 		return u*c;
 			}
 
-	public static double operator% (vec u,vec v){ /* u%v => dot product */
-		return u.x*v.x+u.y*v.y+u.z*v.z;
-			}	
-
-	
-	public static double dot(vec v,vec w){
-		return v.x*w.x+v.y*w.y+v.z*w.z;
-			}
+///Task3 end
 
 
+///Task5
+	//Now we will make dot products for vectors. The => is another way of writing return.
+	public static double dot(vec a, vec b) => a.x*b.x+a.y*b.y+a.z*b.z;
+
+///Task6
+	public static bool approx(double a,double b,double acc=1e-9,double eps=1e-9){
+		if(Abs(a-b)<acc)return true;
+		if(Abs(a-b)<(Abs(a)+Abs(b))*eps)return true;
+		return false;
+		}
+		
 
 
 }
