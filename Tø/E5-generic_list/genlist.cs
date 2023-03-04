c250d82499143ffdc844d111bf4129fbddeafe23
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-03-04 19:04:33 +0100

    Generic list done

--- Tø/E5-generic_list/genlist.cs
+++ Tø/E5-generic_list/genlist.cs
@@ -4,13 +4,14 @@ using static System.Console;
 
 public class genlist<T>{
 	public T[] data;
-	public int size => data.Length;
-	public T this[int i] => data[i];
-	public genlist(){data = new T[0];}
-	public void add(T item){
+	public int size => data.Length; // property
+	public T this[int i] => data[i]; // indexer
+	public genlist(){ data = new T[0]; }
+	public void add(T item){ /* add item to the list */
 		T[] newdata = new T[size+1];
 		System.Array.Copy(data,newdata,size);
 		newdata[size]=item;
-		data = newdata;
+		data=newdata;
 	}
 }
+
--- Tø/E5-generic_list/main.cs
+++ Tø/E5-generic_list/main.cs
@@ -1,26 +1,30 @@
 using System;
-using static System.Console;
 using static System.Math;
-
+using static System.Console;
 
 static public class main{
-	static public void Main(string[] args){
-		var list = new genlist<double[]>();
-			char[] delimiters = {' ','\t'};
-		var options = StringSplitOptions.RemoveEmptyEntries;
-		for(string line = ReadLine(); line!=null; line = ReadLine()){
-			var words = line.Split(delimiters,options);
-			int n = words.Length;
-			var numbers = new double[n];
-			for(int i=0;i<n;i++) numbers[i] = double.Parse(words[i]);
-				list.add(numbers);
-       		}
-		for(int i=0;i<list.size;i++){
-			var numbers = list[i];
-			foreach(var number in numbers)Write($"{number : 0.00e+00;-0.00e+00} ");
-			WriteLine();
-        	}
+	public static void Main(string[] args){
+	
+	var list = new genlist<double[]>(); // make a new list that will be read.
+	
+	foreach(var arg in args){          // look for arg for every argument in inputfile 
+		var words = arg.Split(' ', '\t'); 
+		int n = words.Length;
+		var numbers = new double[n];
+		for(int i=0;i<n;i++){       
+			numbers[i] = double.Parse(words[i]); //it has now made a list with all the numbers in the inputfile
+			}
+		list.add(numbers);
+		}	
+	WriteLine("--------------------------------------------");
+       	WriteLine("number index.| raw value.| value.");
+	WriteLine("--------------------------------------------");
 
+	for(int i=0; i<list.size;i++){
+		var numbers = list[i];
+		foreach(var number in numbers) WriteLine(String.Format("{0,-12} | {1,-9} | {2,5}",$"{i}  ", $"{number}",$"{number : 0.00e+00;-0.00e+00}" ));
+		}
+	WriteLine("--------------------------------------------");
 
 	}
 }
