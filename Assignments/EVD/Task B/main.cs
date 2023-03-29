ba4f533d63ead5ebbd738de168f2c134706973b0
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-03-29 12:40:53 +0200

    Task B EVD done

--- Assignments/EVD/Task B/main.cs
+++ Assignments/EVD/Task B/main.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using static System.Console;
 using static System.Math;
 
 public static class main{
-	
 	public static matrix Ham(double rmax, double dr){
 		int npoints = (int)(rmax/dr)-1;
 		vector r = new vector(npoints);
@@ -30,32 +30,22 @@ public static class main{
 	public static void Main(string[] args){
 		double rmax=10;
 		double dr=0.3;
-		
 
 		foreach(var arg in args){
 			var words = arg.Split(':');
 
-			if(words[0] == "-rmaxs") {
-				for(rmax=2.5; rmax<15;rmax+=0.5){
-					matrix H = Ham(rmax, dr);
-					(var D, var V) = jacobi.cyclic(H);
-                        		vector e = new vector(D.size1);
-					for(int i=0; i<D.size1;i++)
-                              			e[i]=D[i,i];
-					WriteLine($"{rmax} {e[0]}");
-				}
-			}
-			else if(words[0] == "-drs"){
-				for(dr=0.05; dr<2;dr+=0.05){
-					matrix H = Ham(rmax, dr);
-					(var D, var V) = jacobi.cyclic(H);
-                        		vector e = new vector(D.size1);
-					for(int i=0; i<D.size1;i++)
-                                		e[i]=D[i,i];
-					WriteLine($"{dr} {e[0]}");
-				}
-			}
+			if(words[0]=="-rmax"){
+            			rmax = double.Parse(words[1]);
+        		}
+        		if(words[0]=="-dr"){
+           			 dr= double.Parse(words[1]);
+        		}	
 		}	
 
+		matrix H = Ham(rmax, dr);
+		(var D, var V) = jacobi.cyclic(H);
+                vector e = new vector(D.size1);
+		for(int i=0; i<D.size1;i++) e[i]=D[i,i];
+		WriteLine($"{rmax} {dr} {e[0]} {e[1]}");
 	}
 }
