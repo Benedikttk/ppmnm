67e1e6ba09130b5a62670ff6cef4ef3a5ebc176a
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-04-18 13:53:19 +0200

    A is done now

--- Assignments/Splines/main.cs
+++ Assignments/Splines/main.cs
@@ -20,20 +20,30 @@ public class main{
         	double dy=y[i+1]-y[i];
         	return y[i]+dy/dx*(z-x[i]);
         }	
-	
-	public static double linterpInteg(double[] x, double[] y, double z){
-		double integral = 0;
-		int index = x.Length;
-		for(int i=0;i<index;i++){
-			double dx=x[i+1]-x[i]; if(!(dx>0)) throw new Exception("something seams wrong, dx is in incorrect order");
-			double dy=y[i+1]-y[i];
-			double p = dy/dx;
-			double s = y[i]*(dx-x[i])+p*((dx-x[i])*(dx-x[i]))/2;
-			integral += s;
 
+	public static double linterpInteg(double[] x, double[] y, double z){
+		double sum = 0;
+		int index = binsearch(x, z);
+		for(int i=0;i<index;i++){
+			//calculate integral of interval
+			double dx=x[i+1]-x[i]; if(!(dx>0)) throw new Exception("seems wrong again...");
+			double dy=y[i+1]-y[i];
+			double a = dy/dx;
+			double b = y[i]-a*x[i];
+			double integral = (a/2)*(x[i+1]*x[i+1]-x[i]*x[i]) + b*dx;
+			sum += integral;
+			}
+		double last_y = linterp(x, y, z);
+		double last_x = z;
+		double last_dx = last_x-x[index];
+		double last_dy = last_y-y[index];
+		double last_a = last_dy/last_dx;
+		double last_b = y[index]-last_a*x[index];
+		double last_integral = (last_a/2)*(last_x*last_x-x[index]*x[index]) + last_b*last_dx;
+		sum += last_integral;
+		return sum;
 		}
-		return integral;
-	}
+
 	}//func
 	static void Main(string[] args){
 		double[] xs = {1,3,4,7,9,12};
@@ -54,12 +64,12 @@ public class main{
 			}	
 			if(arg == "integral"){
                                 int l = xs.Length;
-                                for(double z=xs[0];z<=xs[xs.Length-1];z+=1.0/10.0){
-                                        double interpValue = funcs.linterp(xs, ys, z);
-                                        WriteLine($"{z} {interpValue}");
-                                }
-			}
+				for(double z=xs[0];z<=xs[xs.Length-1];z+=1.0/10){
+                         	       WriteLine($"{z} {funcs.linterpInteg(xs, ys, z)}");
+                        	}	
+               		}
 		}
+		
 
 	}//Main
 }//main
