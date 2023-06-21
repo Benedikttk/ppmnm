ccf77ef04d106e0b3660b1816784281c8842f6b4
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-06-21 22:28:55 +0200

    It Works whuo whup

--- Eksamen/main.cs
+++ Eksamen/main.cs
@@ -9,8 +9,8 @@ public class main
     public static void Main(string[] args)
     {
 
-        double[] xTestData = {-2,-1,1,2};
-        double[] yTestData = {-1,-1,1,1};
+        double[] xTestData = {1,2,3,4,5,6,7,8,9,10,11};
+        double[] yTestData = {0,0,0,6,3,9,9,2,1,8,5};
 
 	foreach(var arg in args)
 	{
