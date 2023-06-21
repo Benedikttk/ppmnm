0382ea21e9b63bc79977c058e4381fb023e9f723
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-06-21 19:28:01 +0200

    y values are returned as 0

--- Eksamen/main.cs
+++ Eksamen/main.cs
@@ -6,42 +6,41 @@ public class main
 {
     public static void Main(string[] args)
     {
+        double[] xs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        double[] ys = {0, 0, 0, 6, 4, 12, 12, 1, 0.5, 3, 5};
 
-	double[] xs = {1,2,3,4,5,6,7,8,9,10,11};
-        double[] ys = {0,0,0,6,4,12,12,1,0.5,3,5};
+        foreach (var arg in args)
+        {
+		if (arg == "test")
+            {
+                int l = xs.Length;
 
+                for (int i = 0; i < l; i++)
+                {
+                    WriteLine($"{xs[i]+1} {ys[i]+1}");
+                }//for
+            }//if
+        	if(arg=="AkimaSubSplineInetpolation")
+		{
 
+        	// Create an instance of AkimaSpline
+        	AkimaSpline spline = AkimaSpline.Create(xs, ys);
 
-	foreach(var arg in args){
-		if(arg=="test"){
-			int l = xs.Length;
-			
-			for(int i=0;i<l;i++){
-				WriteLine($"{xs[i]+1} {ys[i]+1}");
-			}//for
-		}//if test
-
-		if(arg=="AkimaSubSplineInetpolation"){
-			
-			int numPoints = (int)((xs[xs.Length-1]-xs[0])*10)+1;
-			double[] interpolatedValues = new double[numPoints];
-
-			int index = 0;
-
-			for(double z=xs[0]; z<=xs[xs.Length-1];z+=1.0/10.0)
-			{	
-				double interpValue = spline.Evaluate(z);
-            			interpolatedValues[index] = interpValue;
-            			index++;
-            			WriteLine($"{z} {interpValue}");
-
-			}
+        	// Interpolate a range of values
+        	double start = xs[0];
+        	double end = xs[xs.Length - 1];
+        	double step = 0.05;
+        		for (double value = start; value <= end; value += step)
+        		{	
+            		double interpolated = spline.Evaluate(value);
+            		WriteLine($"{value},  {interpolated}");
+        		}
 		}
-
-
-
-
-        }//foreach
+	
+	
+	
+	
+	}//foreach
     }//Main
 }//main
 
