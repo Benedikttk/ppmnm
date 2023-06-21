06770af9ded8adf425e0dbc5170cdf14386e1911
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-06-21 22:16:33 +0200

    big man progress

--- Eksamen/main.cs
+++ Eksamen/main.cs
@@ -8,23 +8,37 @@ public class main
 {
     public static void Main(string[] args)
     {
+
+        double[] xTestData = {-2,-1,1,2};
+        double[] yTestData = {-1,-1,1,1};
+
 	foreach(var arg in args)
 	{
 		if(arg=="test")
 		{ 
-			double[] xTestData = {1,2,4,5};
-			double[] yTestData = {1,1,2,2};
-
-			AkimaSpline spline = AkimaSpline.Create(xTestData,yTestData); /*Here I have created an instance with an x and y lyst*/
+			for(int i=0;i<xTestData.Length;i++){
+				
+				WriteLine($"{xTestData[i]} {yTestData[i]}");
+			}
+		
+		}//if
+		if(arg=="AkimaSubSplineInetpolation")
+		{	AkimaSpline spline = AkimaSpline.Create(xTestData,yTestData); /*Here I have created an instance with an x and y lyst*/
 			
 			double startInterpValue = xTestData.Min();
-			double endInterpValue = yTestData.Max();
-			for(double dx=startInterpValue; dx<endInterpValue; dx=+1/64)
-				WriteLine(dx);
+			double endInterpValue = xTestData.Max();
 			
+			
+			for(double dx=startInterpValue+1.0/128;dx<=endInterpValue;dx+=1.0/64){
+			
+				double interpolatedValue = spline.Evaluate(dx);
+				WriteLine($"{dx} {interpolatedValue}");
+			}
+		
 
-		}
-	}	
+
+		}//if
+	}//foreach	
 
     }//Main
 }//main
