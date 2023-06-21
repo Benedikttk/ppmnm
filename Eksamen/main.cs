b65f76d1ccf71ce1fff0e8f700374b76a6b4fee3
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-06-21 18:28:10 +0200

    New problem but progress

--- Eksamen/main.cs
+++ Eksamen/main.cs
@@ -7,17 +7,40 @@ public class main
     public static void Main(string[] args)
     {
 
+	double[] xs = {1,2,3,4,5,6,7,8,9,10,11};
+        double[] ys = {0,0,0,6,4,12,12,1,0.5,3,5};
+
+
+
 	foreach(var arg in args){
 		if(arg=="test"){
-			double[] xs = {1,2,3,4,5,6,7,8,9,10,11};
-			double[] ys = {0,0,0,6,4,12,12,1,0.5,3,5};
 			int l = xs.Length;
 			
 			for(int i=0;i<l;i++){
 				WriteLine($"{xs[i]+1} {ys[i]+1}");
 			}//for
 		}//if test
-	
+
+		if(arg=="AkimaSubSplineInetpolation"){
+			
+			int numPoints = (int)((xs[xs.Length-1]-xs[0])*10)+1;
+			double[] interpolatedValues = new double[numPoints];
+
+			int index = 0;
+
+			for(double z=xs[0]; z<=xs[xs.Length-1];z+=1.0/10.0)
+			{	
+				double interpValue = spline.Evaluate(z);
+            			interpolatedValues[index] = interpValue;
+            			index++;
+            			WriteLine($"{z} {interpValue}");
+
+			}
+		}
+
+
+
+
         }//foreach
     }//Main
 }//main
