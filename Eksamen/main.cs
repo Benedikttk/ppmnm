19eee2344b9b909980d800190619a04d9cbd2c94
Benedikt Tor Kristinsson <113021285+Benedikttk@users.noreply.github.com>
2023-06-26 15:39:37 +0200

    Updated and bugs fixed

--- Eksamen/main.cs
+++ Eksamen/main.cs
@@ -11,66 +11,64 @@ public class main
 {
     public static void Main(string[] args)
     {
+        var random = new Random();
+        int dataSize=random.Next(10, 50);
 
-	var random = new Random();
-	int dataSize = random.Next(10, 20);
+        double[] xTestData = new double[dataSize];
+        double[] yTestData = new double[dataSize];
 
-	double[] xTestData = new double[dataSize];
-	double[] yTestData = new double[dataSize];
+        for(int i = 0;i<dataSize; i++)
+        {
+            xTestData[i] = random.NextDouble();
+            yTestData[i] = random.NextDouble();
+        }
 
-	for(int i=0;i<dataSize; i++)
-	{
-    		xTestData[i] = random.NextDouble();
-   		yTestData[i] = random.NextDouble();
-	}
+        Array.Sort(xTestData, yTestData);
 
-	
+        foreach(var arg in args)
+        {
+            if(arg == "test")
+            {
+                for(int i=0;i<dataSize;i++)
+                {
+                    WriteLine($"{xTestData[i]} {yTestData[i]}");
+                }
+            }
+            else if(arg=="AkimaSubSplineInetpolation")
+            {
+                var rnd_data = File.ReadAllText("testpoints.data").Split("\n");
+                int fileDataSize = rnd_data.Length - 1;
+                
+                // Resize arrays if needed
+                if(fileDataSize>dataSize)
+                {
+                    xTestData = new double[fileDataSize];
+                    yTestData = new double[fileDataSize];
+                }
 
-	Array.Sort(xTestData, yTestData);
+                for(int i=0;i<fileDataSize;i++)
+                {
+                    var xys = rnd_data[i].Split(' ');
+                    var x = xys[0].Replace(",", ".");
+                    var y = xys[1].Replace(",", ".");
+                    double.TryParse(x, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out xTestData[i]);
+                    double.TryParse(y, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out yTestData[i]);
+                }
 
+                Array.Sort(xTestData, yTestData);
 
-	foreach(var arg in args)
-	{
-		if(arg=="test")
-		{ 
-			for(int i=0;i<dataSize;i++){
-				
-				WriteLine($"{xTestData[i]} {yTestData[i]}");
-			}
-		
-		}//if
+                AkimaSpline spline = AkimaSpline.Create(xTestData, yTestData);
 
-		if(arg=="AkimaSubSplineInetpolation")
-		{	
-			var rnd_data = File.ReadAllText("testpoints.data").Split("\n");
-    			for (int i = 0; i < rnd_data.Length-1; i++)
-    			{	
-        			var xys = rnd_data[i].Split(' ');
-       				var x = xys[0].Replace(",", ".");
-        			var y = xys[1].Replace(",", ".");
-        			double.TryParse(x, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out xTestData[i]);
-        			double.TryParse(y, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out yTestData[i]);
-            			}
-					
-				
-		        Array.Sort(xTestData, yTestData);
-		
-			AkimaSpline spline = AkimaSpline.Create(xTestData,yTestData);
-			
-			double startInterpValue = xTestData.Min();
-			double endInterpValue = xTestData.Max();
-			
-			
-			for(double dx=startInterpValue+1.0/256;dx<=endInterpValue;dx+=1.0/256){
-			
-				double interpolatedValue = spline.Evaluate(dx);
-				WriteLine($"{dx} {interpolatedValue}");
-			}
-		
+                double startInterpValue = xTestData.Min();
+                double endInterpValue = xTestData.Max();
 
-
-		}//if
-	}//foreach	
+                for(double dx=startInterpValue; dx<=endInterpValue; dx+=1.0/1000)
+                {
+                    double interpolatedValue = spline.Evaluate(dx);
+                    WriteLine($"{dx} {interpolatedValue}");
+                }
+            }
+        }
 
     }//Main
 }//main
